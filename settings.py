c9847cfbdac4304c268a2ab613d084605c8c6bda
Agustin Wojtyszyn <agustinwojtyszyn99@gmail.com>
2025-07-08 14:13:12 -0300

    Corrige configuración y dependencias para despliegue en producción

--- settings.py
+++ settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-replace-this-with-a-secure-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['168.231.99.42', 'localhost', '127.0.0.1']
 
 # Application definition
 
